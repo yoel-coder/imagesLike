a514342c4b639ab579dba67d686595ab0a1f00c9
Yoel Lefkowitz <yoellef218@gmail.com>
2026-05-25 13:35:05 -0400

    changed views

--- imagesLike.web/Models/ImageViewModel.cs
+++ imagesLike.web/Models/ImageViewModel.cs
@@ -4,8 +4,8 @@ namespace imagesLike.web.Models
 {
     public class ImageViewModel
     {
-        public Image image {  get; set; }
+        
         public List<Image> images { get; set; }
-        public List<int> likedImages { get; set; }
+       
     }
 }
